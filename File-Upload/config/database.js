4d99da4dd839c702bd9dd7c7a10c5d7a1192873c
thefearlesscoder <thefunnelvkd@gmail.com>
2024-08-12 19:12:01 +0530

    added codes

---
+++ File-Upload/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+exports.connect = async () => {
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(console.log('Connected to MongoDB'))
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error)
+        console.error(error);
+        process.exit(1);
+    });
+}
